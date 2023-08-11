2fed3c598dd7c1451e4a11cb7ecb2bfc0b602a4e
GeorgiVasilevv <georgi010205@gmail.com>
2023-08-11 20:20:22 +0300

    Added future implementation controller to make it easier for expanding

---
+++ StoreManagementSystem/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManagementSystem.Controllers
+{
+    public class OrderController : BaseController
+    {
+        //For future implementation
+    }
+}
