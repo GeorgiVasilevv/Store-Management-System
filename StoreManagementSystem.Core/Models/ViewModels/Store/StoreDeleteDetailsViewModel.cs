fe2b0719abd3b952704a380ae5c5aab39b5b49de
GeorgiVasilevv <georgi010205@gmail.com>
2023-08-11 19:44:53 +0300

    Minor Fixes

--- StoreManagementSystem.Core/Models/ViewModels/Store/StoreDeleteDetailsViewModel.cs
+++ StoreManagementSystem.Core/Models/ViewModels/Store/StoreDeleteDetailsViewModel.cs
@@ -1,10 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace StoreManagementSystem.Core.Models.ViewModels.Store
 {
     public class StoreDeleteDetailsViewModel
     {
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
+
+        [Display(Name = "Date created")]
         public DateTime DateCreated { get; set; }
         public string ImageUrl { get; set; } = null!;
     }
